cf2be6dc46246ab45c015de47b87940d4ccfae47
16230091@qq.com <16230091@qq.com>
2017-04-20 01:08:03 +0800

    2.4 增加登录Service，不使用依赖注入的方式

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../core/auth.service'
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  service: AuthService;
+
   onClick(userName, password) {
     console.log(userName+','+password)
+
+    let result = this.service.loginWithCredentials(userName, password);
+    console.log('the result is ' + result);
   }
 
-  constructor() { }
+  constructor() { 
+    this.service = new AuthService();
+  }
 
   ngOnInit() {
   }
